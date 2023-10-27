b5e63f4dfde5460f338b9ca6d64eb4cc0b963d81
frtky <fratkayya@gmail.com>
2023-10-27 15:11:22 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_html_game/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_html_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/html.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class hmtlscren extends StatefulWidget {
+  const hmtlscren({super.key});
+
+  @override
+  State<hmtlscren> createState() => _hmtlscrenState();
+}
+
+class _hmtlscrenState extends State<hmtlscren> {
+  late WebViewController _webViewController;
+  String filePath = 'assets/deneme.html';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebView(
+        initialUrl: '',
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _webViewController = webViewController;
+          _loadHtmlFromAssets();
+        },
+      ),
+    );
+  }
+
+  _loadHtmlFromAssets() async {
+    String fileHtmlContents = await rootBundle.loadString(filePath);
+    _webViewController.loadUrl(Uri.dataFromString(fileHtmlContents,
+            mimeType: 'text/html', encoding: Encoding.getByName('utf-8'))
+        .toString());
+  }
+}
